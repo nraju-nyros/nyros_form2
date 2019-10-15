166320562b7086e733c6de913dbab57bb4dbc957
nyros <manaswini.nyros@gmail.com>
2019-10-15 10:51:26 +0530

    Intial commit

--- db/migrate/20191014060751_create_nyros_form2_failures.rb
+++
@@ -1,9 +0,0 @@
-class CreateNyrosForm2Failures < ActiveRecord::Migration[5.2]
-  def change
-    create_table :nyros_form2_failures do |t|
-      t.text :message
-
-      t.timestamps
-    end
-  end
-end
