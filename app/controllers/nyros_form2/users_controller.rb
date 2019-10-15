c0d4ad63482db0beb8fc49597c65e55f8703f8cc
nyros <manaswini.nyros@gmail.com>
2019-10-15 10:23:40 +0530

    intial commit

--- app/controllers/nyros_form2/users_controller.rb
+++ app/controllers/nyros_form2/users_controller.rb
@@ -13,7 +13,6 @@ module NyrosForm2
     def create
       @user = User.new(user_params)
       if @user.save
-
         redirect_to users_path
 
       else   
--- app/models/nyros_form2/user.rb
+++ app/models/nyros_form2/user.rb
@@ -1,14 +1,13 @@
 module NyrosForm2
   class User < ApplicationRecord
   	validates :email,uniqueness: true, format: {
-    with: URI::MailTo::EMAIL_REGEXP, 
-    message: '*Please Enter Your Email'
-  }
-  
-
-
+	    with: URI::MailTo::EMAIL_REGEXP, 
+	    message: '*Please Enter Your Email'
+	  }
     validates_presence_of :name,  message: "*Please Enter Your Name"
-    validates_presence_of :mobile, message: "Please Enter Your Mobile"
+    # svalidates_presence_of :mobile, message: "*Please Enter Your Mobile"
+    validates_format_of :mobile, :with =>  /\d[0-9]\)*\z/ , :message => "*Please Enter a Valid Mobile Number"
+
 
 
   end
