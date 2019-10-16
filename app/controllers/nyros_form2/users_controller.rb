08f1e0c7a0e3a41c80688335a05e8ca9f7d8adca
nyros <manaswini.nyros@gmail.com>
2019-10-16 17:53:46 +0530

    Update validations

--- app/controllers/nyros_form2/users_controller.rb
+++ app/controllers/nyros_form2/users_controller.rb
@@ -3,7 +3,7 @@ require_dependency "nyros_form2/application_controller"
 module NyrosForm2
   class UsersController < ApplicationController
     def index
-        @users = User.all
+      @users = User.all
     end
 
     def new
@@ -12,14 +12,18 @@ module NyrosForm2
 
     def create
       @user = User.new(user_params)
-      if @user.save
-        redirect_to users_path
-
-      else   
-       render new_user_path
-      end   
+      respond_to do |format|
+        if @user.save
+          format.html { redirect_to users_path, notice: 'Contact was successfully created.' }
+          format.json { render json: @user, status: :created, location: @user }
+        else
+          format.html { render action: "new" }
+          format.json { render json: @user.errors, status: :unprocessable_entity }
+        end
+      end
     end
-   
+
+
 
     def show
       @user = User.find(params[:id])
@@ -47,9 +51,10 @@ module NyrosForm2
         format.js  { render :layout => false }
       end    
     end   
-
-    def user_params   
-      params.require(:user).permit(:name, :email, :mobile);  
-    end  
+    
+    private
+    def user_params 
+      params.permit(:name, :email, :mobile) 
+    end
   end
 end
--- lib/nyros_form2/version.rb
+++ lib/nyros_form2/version.rb
@@ -1,3 +1,3 @@
 module NyrosForm2
-  VERSION = '0.2'
+  VERSION = '0.2.3'
 end
